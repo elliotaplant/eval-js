126e690e4f3be3e3cf3e64cdb06892618d552b3c
Elliot Plant <elliotaplant@gmail.com>
2022-09-27 10:29:56 -0700

    Make extension

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,22 @@
 import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('eval-js.eval', () => {
-		vscode.window.showInformationMessage('Hello World from eval-js!');
-	});
+	context.subscriptions.push(vscode.commands.registerCommand('eval-js.eval', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+		const selections = editor.selections;
 
-	context.subscriptions.push(disposable);
+		try {
+			const results = selections.map(selection => eval(editor.document.getText(selection)).toString());
+			editor.edit(editBuilder => {
+				selections.forEach((selection, i) => editBuilder.replace(selection, results[i]));
+			});
+		} catch (e: any) {
+		 vscode.window.showErrorMessage(e.toString());
+		}
+	}));
 }
 
 // this method is called when your extension is deactivated
